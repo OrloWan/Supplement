3ed435196b387ba14b00b99c3997e5383f8266a5
OrloWan <orlowan228@ya.ru>
2019-11-27 00:03:23 +0300

    сверстал секцию realman

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,14 @@
-
+$('.realman-slider').slick({
+	dots: true,
+	infinite: true,
+  responsive: [
+    {
+      breakpoint: 993,
+      settings: {
+        infinite: true,
+				dots: true,
+				arrows: false
+      }
+    }
+  ]
+})
